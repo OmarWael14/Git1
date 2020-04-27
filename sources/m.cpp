f87d7a71c63e64d1d47d62e82ed39e5b005a33c8
mohannadmonir <mohannadmonir@aucegypt.edu>
2020-04-28 00:24:53 +0200

    message

---
+++ sources/m.cpp
@@ -0,0 +1,10 @@
+
+#include"m.h"
+#include<iostream>
+using namespace std;
+void print()
+{
+	cout<<"My username is mohannadmonir"<<endl;
+	cout<<"My email is mohannadmonir@aucegypt.edu"<<endl;
+
+}
